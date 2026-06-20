6c2ed4c8c2efe9c581ac791424a331ebf94204aa
Nahuel_Savoy <savoynahuel9@gmail.com>
2026-06-19 21:17:25 -0300

    Update index.py
    actualizo index

--- index.py
+++ index.py
@@ -11,7 +11,8 @@ from Peleas.sprite_combate import batalla
 from Mundo_libre.Escenario_mundo import escenario_mundo
 import Personaje
 import estilo
-
+from TIENDA.tienda import Tienda
+from TIENDA.mostrador import mostrador
 
 ventana = pygame.display.set_mode((1064, 704))
 
@@ -31,7 +32,9 @@ while ejecutar:
     elif estado == "SELECCION":
 
         profe_elegido = menu_seleccion(ventana)
-        jugador = Personaje.personaje(os.path.join(estilo.DIRECTORIO_BASE, f"img/skins/{profe_elegido}/OV.png"))
+        jugador = Personaje.personaje(os.path.join(estilo.DIRECTORIO_BASE, f"img/skins/{profe_elegido}/OV.png")) #creamos al jugador
+        tienda = Tienda() #creamos a la tienda
+
 
         estado = "MUNDO_LIBRE"
 
@@ -43,20 +46,22 @@ while ejecutar:
 
 
     elif estado == "TIENDA":
+        
         accion_jugador = escenario_tienda(ventana,jugador)
 
         if accion_jugador == "SALIR":
             estado = "MUNDO_LIBRE"
 
         elif accion_jugador == "COMPRAR":
-            pass
-
+            estado = mostrador(ventana,jugador,tienda)
+           
+ 
 
     elif estado == "PELEANDO":
 
         resultado_pelea = batalla()
         if resultado_pelea == "ganaste":
-            billetera = None #aca sumar plata al jugador instanciado
+            billetera = None #aca sumar plata al jugador instanciado o hacerlo dentro de batalla()
         estado = resultado_pelea
 
 
